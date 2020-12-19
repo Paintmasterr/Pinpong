47ebb07993704458611b16dc987727a71b553afa
AkifCanDUMAN <73740265+AkifCanDUMAN@users.noreply.github.com>
2020-12-19 19:31:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,121 +1,136 @@
 import turtle
 import winsound
 
-window = turtle.Screen()
-canvas = window.getcanvas()
+win = turtle.Screen()
+canvas = win.getcanvas()
 root = canvas.winfo_toplevel()
-window.title("PinPong")
-window.bgcolor("orange")
-window.setup(width=800, height=600)
-window.tracer(0)
+win.title('Pong by')
+win.bgcolor('darkblue')
+win.setup(width=800, height=600)
+win.tracer(0)
 
-racket_a = turtle.Turtle()
-racket_a.speed(0)
-racket_a.shape('square')
-racket_a.color('black')
-racket_a.penup()
-racket_a.goto(-350, 0)
-racket_a.shapesize(5, 1)
+score_a = 0
+score_b = 0
 
-racket_b = turtle.Turtle()
-racket_b.speed(0)
-racket_b.shape('square')
-racket_b.color('white')
-racket_b.penup()
-racket_b.goto(350, 0)
-racket_b.shapesize(5, 1)
+a = turtle.Turtle()
+a.speed(0)
+a.shape('square')
+a.color('red')
+a.shapesize(stretch_wid=5, stretch_len=1)
+a.penup()
+a.goto(-350, 0)
+
+b = turtle.Turtle()
+b.speed(0)
+b.shape('square')
+b.color('white')
+b.shapesize(stretch_wid=5, stretch_len=1)
+b.penup()
+b.goto(350, 0)
 
 ball = turtle.Turtle()
-ball.speed(0)
+ball.speed()
 ball.shape('circle')
-ball.color('red')
+ball.color('orange')
 ball.penup()
-ball.dx = 0.15
-ball.dy = 0.15
+ball.goto(0, 0)
+ball.dx = 0.8
+ball.dy = -0.8
 
-write = turtle.Turtle()
-write.speed(0)
-write.color('white')
-write.penup()
-write.goto(0, 260)
-write.write("Player A:0   Player B:0",
-            align='center', font=('Courier', 24, 'bold'))
-write.hideturtle()
-point_a = 0
-point_b = 0
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0  AI: 0', align="center", font=("Courier", 24, "italic"))
 
 
-def racket_a_up():
-    y = racket_a.ycor()
-    y = y + 20
-    racket_a.sety(y)
+def a_up():
+    y = a.ycor()
+    y += 20
+    a.sety(y)
 
 
-def racket_a_down():
-    y = racket_a.ycor()
-    y = y - 20
-    racket_a.sety(y)
+def a_down():
+    y = a.ycor()
+    y -= 20
+    a.sety(y)
 
 
-def racket_b_up():
-    y = racket_b.ycor()
-    y = y + 20
-    racket_b.sety(y)
+def b_up():
+    y = b.ycor()
+    y += 20
+    b.sety(y)
 
 
-def racket_b_down():
-    y = racket_b.ycor()
-    y = y - 20
-    racket_b.sety(y)
-
-
+def b_down():
+    y = b.ycor()
+    y -= 20
+    b.sety(y)
 def close():
     global Continue
     Continue = False
 
-window.listen()
-window.onkeypress(racket_a_up, 'w')
-window.onkeypress(racket_a_down, 's')
-window.onkeypress(racket_b_up, 'Up')
-window.onkeypress(racket_b_down, 'Down')
-
+win.listen()
+win.onkeypress(a_up, "w")
+win.onkeypress(a_down, "s")
+win.onkeypress(b_up, "Up")
+win.onkeypress(b_down, "Down")
 root.protocol("WM_DELETE_WINDOW", close)
 Continue = True
 
 while Continue:
-    window.update()
+    win.update()
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    if ball.ycor() > 290 or ball.ycor() < -290:
-        ball.dy = ball.dy * -1
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
 
     if ball.xcor() > 390:
-        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
         ball.goto(0, 0)
-        ball.dx = ball.dx * -1
-        point_a = point_a + 1
-        write.clear()
-        write.write("Player A:{}   Player B:{}".format(point_a, point_b),
-                    align='center', font=('Courier', 24, 'bold'))
-    if ball.xcor() < -390:
-        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
-        ball.goto(0, 0)
-        ball.dx = ball.dx * -1
-        point_b = point_b + 1
-        write.clear()
-        write.write("Player A:{}   Player B:{}".format(point_a, point_b),
-                    align='center', font=('Courier', 24, 'bold'))
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f'Player A: {score_a} AI: {score_b}', align="center", font=("Courier", 24, "italic"))
 
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (
-            ball.ycor() < racket_b.ycor() + 60 and
-            ball.ycor() > racket_b.ycor() - 60):
-        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
-        ball.setx(340)
-        ball.dx = ball.dx * -1
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (
-            ball.ycor() < racket_a.ycor() + 60 and
-            ball.ycor() > racket_a.ycor() - 60):
-        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
-        ball.setx(-340)
-        ball.dx = ball.dx * -1
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f'Player A:{score_a} AI: {score_b}', align="center", font=("Courier", 24, "italic"))
+
+    if a.ycor() > 260:
+        a.goto(-350, 260)
+
+    if a.ycor() < -240:
+        a.goto(-350, -240)
+
+    if b.ycor() > 260:
+        b.goto(350, 260)
+
+    if b.ycor() < -240:
+        b.goto(350, -240)
+
+    if ball.xcor() > 330 and ball.xcor() < 340 and (ball.ycor() < b.ycor() + 40 and ball.ycor() > b.ycor() - 40):
+        ball.setx(330)
+        ball.dx *= -1
+        winsound.PlaySound("pongbounce.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() < -330 and ball.xcor() > -340 and (ball.ycor() < a.ycor() + 40 and ball.ycor() > a.ycor() - 40):
+        ball.setx(-330)
+        ball.dx *= -1
+        winsound.PlaySound("pongbounce.wav", winsound.SND_ASYNC)
+
+    if b.ycor() < ball.ycor() and abs(b.ycor() - ball.ycor()) > 10:
+        b_up()
+
+    elif b.ycor() > ball.ycor() and abs(b.ycor() - ball.ycor()) > 10:
+        b_down()
